9ea811a0b6d59666f94045be9f5b6cf322cc21c1
Roman Tsypuk <tsypuk.conf@gmail.com>
2023-05-17 19:45:54 +0300

    chore: updated samples; added sample for reading yaml

---
+++ samples/samples/aws_iam_from_yaml.py
@@ -0,0 +1,17 @@
+from multicloud_diagrams import MultiCloudDiagrams
+
+
+def main():
+    mcd = MultiCloudDiagrams()
+
+    prefix = 'prod'
+    output_file = f'../output/output.{prefix}.iam-roles-from-yaml.drawio'
+    mcd.read_coords_from_file(output_file)
+
+    # Read cloud resources declared in yaml format
+    mcd.augment_from_yaml('augmented_resources.yaml')
+    mcd.export_to_file(output_file)
+
+
+if __name__ == "__main__":
+    main()
