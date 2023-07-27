ca519bf041ba9482fc15dac0dc6a1c488972a541
Roman Tsypuk <tsypuk.conf@gmail.com>
2023-07-27 11:58:49 +0300

    tests: drawio diagram

--- tests/test_mcd.py
+++ tests/test_mcd.py
@@ -19,3 +19,16 @@ class TestMultiCloudDiagramsDrawIO(TestCase):
         self.assertEqual('multicloud-diagrams', root.attrib['host'])
         self.assertEqual('PIP package multicloud-diagrams. Generate resources in draw.io compatible format for Cloud infrastructure. Copyrights @ Roman Tsypuk 2023. MIT license.', root.attrib['agent'])
         self.assertEqual('MultiCloud', root.attrib['type'])
+
+    def test_diagram_mx_graph_model(self):
+        # given
+        mcd = MultiCloudDiagrams()
+
+        # when
+        tree = et.ElementTree(mcd.mxfile)
+        diagram = tree.find("./")
+
+        # then
+        self.assertEqual('diagram', diagram.tag)
+        self.assertEqual('diagram_1', diagram.attrib['id'])
+        self.assertEqual('AWS components', diagram.attrib['name'])
