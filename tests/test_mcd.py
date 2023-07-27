e8e5b904fd7e0fd5c13095cea5edf19e62bda286
Roman Tsypuk <tsypuk.conf@gmail.com>
2023-07-27 12:27:05 +0300

    tests: drawio mxcell

--- tests/test_mcd.py
+++ tests/test_mcd.py
@@ -4,7 +4,7 @@ from multicloud_diagrams import MultiCloudDiagrams
 import xml.etree.ElementTree as et
 
 
-class TestMultiCloudDiagramsDrawIO(TestCase):
+class TestMultiCloudDiagramsDefaultDrawIO(TestCase):
 
     def test_drawio_preambula(self):
         # given
@@ -12,12 +12,15 @@ class TestMultiCloudDiagramsDrawIO(TestCase):
 
         # when
         tree = et.ElementTree(mcd.mxfile)
-        root = tree.find(".")
+        roots = tree.findall(".")
+        root = roots[0]
 
         # then
+        self.assertEqual(1, len(roots))
         self.assertEqual('mxfile', root.tag)
         self.assertEqual('multicloud-diagrams', root.attrib['host'])
-        self.assertEqual('PIP package multicloud-diagrams. Generate resources in draw.io compatible format for Cloud infrastructure. Copyrights @ Roman Tsypuk 2023. MIT license.', root.attrib['agent'])
+        self.assertEqual('PIP package multicloud-diagrams. Generate resources in draw.io compatible format for Cloud infrastructure. Copyrights @ Roman Tsypuk 2023. MIT license.',
+                         root.attrib['agent'])
         self.assertEqual('MultiCloud', root.attrib['type'])
 
     def test_diagram(self):
@@ -26,9 +29,11 @@ class TestMultiCloudDiagramsDrawIO(TestCase):
 
         # when
         tree = et.ElementTree(mcd.mxfile)
-        diagram = tree.find("./")
+        diagrams = tree.findall("./")
+        diagram = diagrams[0]
 
         # then
+        self.assertEqual(1, len(diagrams))
         self.assertEqual('diagram', diagram.tag)
         self.assertEqual('diagram_1', diagram.attrib['id'])
         self.assertEqual('AWS components', diagram.attrib['name'])
@@ -39,13 +44,16 @@ class TestMultiCloudDiagramsDrawIO(TestCase):
 
         # when
         tree = et.ElementTree(mcd.mxfile)
-        mx_graph_model = tree.find("./diagram/")
+        # ./diagram/
+        mx_graph_models = tree.findall("/*/")
+        mx_graph_model = mx_graph_models[0]
 
         # then
+        self.assertEqual(1, len(mx_graph_models))
         self.assertEqual('mxGraphModel', mx_graph_model.tag)
-        expected = {'dx': '1015', 'dy': '661', 'grid': '1', 'gridSize': '10', 'guides': '1', 'tooltips': '1', 'connect': '1', 'arrows': '1', 'fold': '1', 'page': '1', 'pageScale': '1', 'pageWidth': '850', 'pageHeight': '1100', 'math': '0', 'shadow': '0'}
+        expected = {'dx': '1015', 'dy': '661', 'grid': '1', 'gridSize': '10', 'guides': '1', 'tooltips': '1', 'connect': '1', 'arrows': '1', 'fold': '1', 'page': '1', 'pageScale': '1',
+                    'pageWidth': '850', 'pageHeight': '1100', 'math': '0', 'shadow': '0'}
         self.assertEqual(expected, mx_graph_model.attrib)
-
 
     def test_root(self):
         # given
@@ -53,11 +61,30 @@ class TestMultiCloudDiagramsDrawIO(TestCase):
 
         # when
         tree = et.ElementTree(mcd.mxfile)
-        root = tree.find("./diagram/mxGraphModel/root")
-        # root = tree.find("./diagram/mxGraphModel/*")
-        # TODO check that it is single element
+        # ./diagram/mxGraphModel/root
+        roots = tree.findall("./*/*/*")
+        root = roots[0]
 
         # then
+        self.assertEqual(1, len(roots))
         self.assertEqual('root', root.tag)
         expected = {}
         self.assertEqual(expected, root.attrib)
+
+    def test_mx_cell(self):
+        # given
+        mcd = MultiCloudDiagrams()
+
+        # when
+        tree = et.ElementTree(mcd.mxfile)
+        # ./diagram/mxGraphModel/root/mxCell
+        mxcells = tree.findall("./*/*/*/*")
+
+        # then
+        self.assertEqual(2, len(mxcells))
+        self.verify_mx_cell(mxcells[0], expected={'id': '0'})
+        self.verify_mx_cell(mxcells[1], expected={'id': '1', 'parent': '0'})
+
+    def verify_mx_cell(self, mx_cell, expected):
+        self.assertEqual('mxCell', mx_cell.tag)
+        self.assertEqual(expected, mx_cell.attrib)
