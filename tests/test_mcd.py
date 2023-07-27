bd9caf7b24e1b7d726ec6c8b9eb89b96c98c2f52
Roman Tsypuk <tsypuk.conf@gmail.com>
2023-07-27 12:06:38 +0300

    tests: drawio mxgraphmodel

--- tests/test_mcd.py
+++ tests/test_mcd.py
@@ -20,7 +20,7 @@ class TestMultiCloudDiagramsDrawIO(TestCase):
         self.assertEqual('PIP package multicloud-diagrams. Generate resources in draw.io compatible format for Cloud infrastructure. Copyrights @ Roman Tsypuk 2023. MIT license.', root.attrib['agent'])
         self.assertEqual('MultiCloud', root.attrib['type'])
 
-    def test_diagram_mx_graph_model(self):
+    def test_diagram(self):
         # given
         mcd = MultiCloudDiagrams()
 
@@ -32,3 +32,16 @@ class TestMultiCloudDiagramsDrawIO(TestCase):
         self.assertEqual('diagram', diagram.tag)
         self.assertEqual('diagram_1', diagram.attrib['id'])
         self.assertEqual('AWS components', diagram.attrib['name'])
+
+    def test_mx_graph_model(self):
+        # given
+        mcd = MultiCloudDiagrams()
+
+        # when
+        tree = et.ElementTree(mcd.mxfile)
+        mx_graph_model = tree.find("./diagram/")
+
+        # then
+        self.assertEqual('mxGraphModel', mx_graph_model.tag)
+        expected = {'dx': '1015', 'dy': '661', 'grid': '1', 'gridSize': '10', 'guides': '1', 'tooltips': '1', 'connect': '1', 'arrows': '1', 'fold': '1', 'page': '1', 'pageScale': '1', 'pageWidth': '850', 'pageHeight': '1100', 'math': '0', 'shadow': '0'}
+        self.assertEqual(expected, mx_graph_model.attrib)
