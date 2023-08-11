130a6fc316f665046f138df4a41eedccb93eb471
Roman Tsypuk <tsypuk.conf@gmail.com>
2023-08-11 20:10:32 +0300

    feat: ruby plugin root-include-snippet now able to read content based on func name

--- docs/_plugins/root-include-snippet.rb
+++ docs/_plugins/root-include-snippet.rb
@@ -5,28 +5,45 @@ class RootIncludeSnippet < Liquid::Tag
   end
 
   def render(context)
-    expanded_path = Liquid::Template.parse(@markup).render(context)
+    @func = ''
+    @dir = @markup
+    if (@markup.include?(" "))
+      @dir = @markup.split[0]
+      @func = @markup.split[1]
+    end
+
+    expanded_path = Liquid::Template.parse(@dir).render(context)
     root_path = File.expand_path(context.registers[:site].config['source'])
     final_path = File.join(root_path, expanded_path)
     file_read_opts = context.registers[:site].file_read_opts
 
-    read_lines_from_file(final_path, file_read_opts)
+    read_lines_from_file(final_path, file_read_opts, @func)
   end
 
-  def read_lines_from_file(file_path, file_read_opts)
+  def read_lines_from_file(file_path, file_read_opts, func_name)
     lines = []
     in_section = false
+    in_func = false
+    func = 'def test_' + func_name
+
+    # print(func)
 
     File.foreach(file_path, **file_read_opts) do |line|
       # print(file_path)
-      if line.include?('# given')
-        in_section = true
-      elsif line.include?('# then')
-        break
+      if line.include?(func)
+        in_func = true
       end
-      # line.sub!(/^\t{2}/, "")
-      line.sub!(/^ {8}/, "")
-      lines << line if in_section
+
+      if in_func
+        if line.include?('# given')
+          in_section = true
+        elsif line.include?('# then')
+          break
+        end
+        # line.sub!(/^\t{2}/, "")
+        line.sub!(/^ {8}/, "")
+        lines << line if in_section and in_func
+      end
     end
     # print(lines)
     # lines << '# then'
