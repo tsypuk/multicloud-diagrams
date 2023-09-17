74992aaa92d0d6db6e0e2678dedf287181ca639a
Roman Tsypuk <tsypuk.conf@gmail.com>
2023-09-17 13:24:18 +0300

    refactor: updated client vpn

--- tests/aws/test_client_vpn.py
+++ tests/aws/test_client_vpn.py
@@ -15,7 +15,6 @@ class TestAWSVertexInIsolation(TestRendering):
         vpn_arn = 'aws:clientvpn:us-west-1:prod/cvpn-endpoint-0e3b'
         vpn_name = 'vpc-client-vpn'
         metadata = {
-            'client-vpn endpoint': 'ARN: aws:clientvpn:us-west-1:prod/cvpn-endpoint-0e3b',
             'SplitTunnel': False,
             'DNSServer': '8.8.8.8',
             'VPNProtocol': 'openVPN',
@@ -33,9 +32,7 @@ class TestAWSVertexInIsolation(TestRendering):
                     'parent': '1',
                     'style': 'sketch=0;outlineConnect=0;fontColor=#232F3E;gradientColor=#945DF2;gradientDirection=north;fillColor=#5A30B5;strokeColor=#ffffff;dashed=0;verticalLabelPosition=bottom;verticalAlign=top;align=left;html=1;fontSize=12;fontStyle=0;aspect=fixed;shape=mxgraph.aws4.resourceIcon;resIcon=mxgraph.aws4.client_vpn;',
                     'value': '<b>Name</b>: vpc-client-vpn<BR><b>ARN</b>: '
-                             'aws:clientvpn:us-west-1:prod/cvpn-endpoint-0e3b<BR>-----------<BR><b>client-vpn '
-                             'endpoint</b>: ARN: '
-                             'aws:clientvpn:us-west-1:prod/cvpn-endpoint-0e3b<BR><b>SplitTunnel</b>: '
+                             'aws:clientvpn:us-west-1:prod/cvpn-endpoint-0e3b<BR>-----------<BR><b>SplitTunnel</b>: '
                              'False<BR><b>DNSServer</b>: 8.8.8.8<BR><b>VPNProtocol</b>: '
                              'openVPN<BR><b>Transport</b>: UDP<BR><b>VPNPort</b>: '
                              '443<BR><b>ClientCertificateARN</b>: '
