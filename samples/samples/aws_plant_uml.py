4c5800c6d9626f4236b50b183e48d5f6d653e9df
Roman Tsypuk <tsypuk.conf@gmail.com>
2023-11-18 05:52:52 +0200

    feat: add parsing of plantUML format

---
+++ samples/samples/aws_plant_uml.py
@@ -0,0 +1,59 @@
+from multicloud_diagrams import MultiCloudDiagrams
+from samples.samples.aws_service_end_2_end import prepare_end2end
+
+
+def main():
+    mcd = MultiCloudDiagrams()
+
+    prefix = 'prod'
+    prev_file = f'../output/output.{prefix}_plantuml.end2end.drawio'
+
+    mcd.read_coords_from_file(prev_file)
+    prepare_end2end(mcd)
+
+    mcd.read_uml_mappings('uml_mapping.yml')
+    label_style = {
+        'labelBackgroundColor': 'none',
+        'fontColor': '#FF3333',
+        'fontStyle': 0,
+        'fontSize': 14
+    }
+    style1 = {
+        'orthogonalLoop': '1',
+        'edgeStyle': 'orthogonalEdgeStyle',
+        'curved': '1',
+        'startArrow': 'oval',
+        'endArrow': 'classicThin',
+        'dashed': '1',
+        'strokeColor': '#FF3333',
+        'strokeWidth': '3',
+        'fontSize': '22'
+    }
+    mcd.read_uml_from_file('file_upload.plantuml', edge_style=style1, label_style=label_style)
+
+    style2 = {
+        'orthogonalLoop': '1',
+        'edgeStyle': 'orthogonalEdgeStyle',
+        'curved': '1',
+        'startArrow': 'oval',
+        'endArrow': 'classicThin',
+        'dashed': '1',
+        'strokeColor': '#0000FF',
+        'strokeWidth': '3',
+        'fontSize': '22'
+    }
+
+    label_style = {
+        'labelBackgroundColor': 'none',
+        'fontColor': '#00FF00',
+        'fontStyle': 0,
+        'fontSize': 14
+    }
+    mcd.read_uml_from_file('process.plantuml', edge_style=style2, label_style=label_style)
+
+    result_file = f'../output/output.{prefix}_plantuml.end2end.drawio'
+    mcd.export_to_file(result_file)
+
+
+if __name__ == "__main__":
+    main()
