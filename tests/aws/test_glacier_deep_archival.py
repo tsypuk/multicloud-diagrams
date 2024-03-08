358bbf263da6fca206a528e4ab00335aa9a7ad29
Roman Tsypuk <tsypuk.conf@gmail.com>
2024-03-08 14:03:53 +0200

    feat: add glacier deep archival

---
+++ tests/aws/test_glacier_deep_archival.py
@@ -0,0 +1,42 @@
+from multicloud_diagrams import MultiCloudDiagrams
+from utils.templating import TestRendering
+
+
+class TestAWSVertexInIsolation(TestRendering):
+
+    def test_glacier(self):
+        # docs
+        self.node_type = 'glacier_deep_archival'
+
+        # given
+        mcd = MultiCloudDiagrams()
+
+        # when
+        glacier_arn = 'arn:aws:glacier:::content_bucket'
+
+        metadata = {
+            'x-amz-bucket-region': 'eu-west-1',
+            'CreationDate': '2023-11-11',
+            'AbortIncompleteMultipartUpload': "{'DaysAfterInitiation': 5}",
+            'Expiration': "{'Days': 90}",
+            'MFADelete': 'Disabled'
+        }
+
+        mcd.add_vertex(node_id=glacier_arn, node_name='content_bucket', node_type='glacier_deep_archival', metadata=metadata)
+
+        # then
+        expected = {'id': 'vertex:glacier_deep_archival:arn:aws:glacier:::content_bucket',
+                    'parent': '1',
+                    'style': 'sketch=0;outlineConnect=0;gradientColor=none;fillColor=#3F8624;strokeColor=none;dashed=0;verticalLabelPosition=bottom;verticalAlign=top;align=center;html=1;fontSize=12;fontStyle=0;aspect=fixed;pointerEvents=1;shape=mxgraph.aws4.glacier_deep_archive;',
+                    'value': '<b>Name</b>: content_bucket<BR><b>ARN</b>: '
+                             'arn:aws:glacier:::content_bucket<BR>-----------<BR><b>x-amz-bucket-region</b>: '
+                             'eu-west-1<BR><b>CreationDate</b>: '
+                             '2023-11-11<BR><b>AbortIncompleteMultipartUpload</b>: '
+                             "{'DaysAfterInitiation': 5}<BR><b>Expiration</b>: {'Days': "
+                             '90}<BR><b>MFADelete</b>: Disabled',
+                    'vertex': '1'}
+
+        self.verify_resource(expected, mcd.mx_file, 'content_bucket', 'glacier_deep_archival')
+
+        # docs
+        self.mcd = mcd
