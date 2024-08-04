1187c3e9b21ff64e1d79f495f35e2fb8f33ba422
Roman Tsypuk <tsypuk.conf@gmail.com>
2024-08-04 11:23:42 +0300

    feat: added aws cloud wan node

--- tests/aws2024/test_cloud_wan.py
+++ tests/aws2024/test_cloud_wan.py
@@ -12,27 +12,26 @@ class TestAWSVertexInIsolation(TestRendering):
         mcd = MultiCloudDiagrams()
 
         # when
-        private_5g_service_arn = 'arn:aws:private_5g:us-west-1:123456789012:service/private_5g/123'
-        private_5g_name = 'Private 5g'
+        cloud_wan_service_arn = 'arn:aws:cloud_wan:us-west-1:123456789012:service/cloud_wan/123'
+        cloud_wan_name = 'Private 5g'
         metadata = {
-            'geolocation-lat': 44,
-            'geolocation-lon': 33,
-            'license-application-date': '02-08-2024',
-            'inter-connection-provider': 'Verizon'
+            'ASN-interconnect': 4405,
+            'ANS': 3301,
+            'edge-location': 'us-east-1',
+            'attachment': 'Site-to-Site-ID'
         }
-        mcd.add_vertex(node_id=private_5g_service_arn, node_name=private_5g_name, node_type='private_5g', metadata=metadata)
+        mcd.add_vertex(node_id=cloud_wan_service_arn, node_name=cloud_wan_name, node_type='cloud_wan', metadata=metadata)
 
         # then
-        expected = {'id': 'vertex:private_5g:arn:aws:private_5g:us-west-1:123456789012:service/private_5g/123',
+        expected = {'id': 'vertex:cloud_wan:arn:aws:cloud_wan:us-west-1:123456789012:service/cloud_wan/123',
                     'parent': '1',
-                    'style': 'sketch=0;outlineConnect=0;fontColor=#232F3E;fillColor=#8C4FFF;strokeColor=#ffffff;dashed=0;verticalLabelPosition=bottom;verticalAlign=top;align=left;html=1;fontSize=12;fontStyle=0;aspect=fixed;shape=mxgraph.aws4.resourceIcon;resIcon=mxgraph.aws4.private_5g;',
+                    'style': 'sketch=0;outlineConnect=0;fontColor=#232F3E;fillColor=#8C4FFF;strokeColor=#ffffff;dashed=0;verticalLabelPosition=bottom;verticalAlign=top;align=left;html=1;fontSize=12;fontStyle=0;aspect=fixed;shape=mxgraph.aws4.resourceIcon;resIcon=mxgraph.aws4.cloud_wan;',
                     'value': '<b>Name</b>: Private 5g<BR><b>ARN</b>: '
-                             'arn:aws:private_5g:us-west-1:123456789012:service/private_5g/123<BR>-----------<BR><b>geolocation-lat</b>: '
-                             '44<BR><b>geolocation-lon</b>: '
-                             '33<BR><b>license-application-date</b>: '
-                             '02-08-2024<BR><b>inter-connection-provider</b>: Verizon',
+                             'arn:aws:cloud_wan:us-west-1:123456789012:service/cloud_wan/123<BR>-----------<BR><b>ASN-interconnect</b>: '
+                             '4405<BR><b>ANS</b>: 3301<BR><b>edge-location</b>: '
+                             'us-east-1<BR><b>attachment</b>: Site-to-Site-ID',
                     'vertex': '1'}
-        self.verify_resource(expected, mcd.mx_file, 'private_5g', self.node_type)
+        self.verify_resource(expected, mcd.mx_file, 'cloud_wan', self.node_type)
 
         # docs
         self.mcd = mcd
